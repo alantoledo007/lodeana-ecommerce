b3b15f0802b8f8ab463798656f4fa63a9a20b60b
Alan Toledo <alantoledo.work@gmail.com>
2022-01-01 20:59:18 -0300

    credits

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,17 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
+      <div className="my-12 text-center">
+        <p className="text-gray-400">
+          Creador por{" "}
+          <a
+            className="text-inherit hover:text-green-500"
+            href="https://mvp-skeleton.com.ar"
+          >
+            <strong>MVP Skeleton</strong>
+          </a>
+        </p>
+      </div>
     </UserProvider>
   );
 }
